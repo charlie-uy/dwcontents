8eb3ff6186de99d3c24d8348223daf1db250305b
charlie-uy <caeruy@gmail.com>
2023-12-16 15:36:38 -0300

    231216 - Wiki commit inicial con gitignore agregado

---
+++ lib/plugins/authldap/lang/pt/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Jose Luis Sanches <jose.reis.sanches@gmail.com>
+ * @author Paulo Carmino <contato@paulocarmino.com>
+ */
+$lang['connectfail']           = 'Não foi possível conectar o LDAP: %s';
+$lang['domainfail']            = 'O LDAP não encontrou seu utilizador';
